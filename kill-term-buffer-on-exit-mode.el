beb64c903227682c809798abd3a00c44516da934
Eric Crosson <eric.s.crosson@utexas.com>
2018-11-05 16:07:25 -0600

    Publish version 1.0.0

--- kill-term-buffer-on-exit-mode.el
+++ kill-term-buffer-on-exit-mode.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 2018  Free Software Foundation, Inc.
 ;;
 ;; Author: Eric Crosson <eric.s.crosson@utexas.com>
-;; Version: 0.0.1
+;; Version: 1.0.0
 ;; Keywords: convenience
 ;; URL: https://github.com/EricCrosson/kill-term-buffer-on-exit-mode
 ;; Package-Requires: ((emacs "24"))
